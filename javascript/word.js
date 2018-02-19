0438980c168e703aff2ec910ab2082977bdb724a
unknown <jjhernandez94@yahoo.com>
2018-02-19 14:53:12 -0800

    fixed syntax errors

--- javascript/word.js
+++ javascript/word.js
@@ -4,30 +4,31 @@
 --------------------------------------------------------------------------------------*/
 var MatchWord = function(hangman_word) {
     // An array of new Letter objects representing the letters of the underlying word
-    underlyingWord = function() {
-        var splitWord = hangman_word.split("");
-    },    
-/*-----------------------------------------------------------------------------------
-     A function that returns a string representing the word. This should call the 
-    function on each letter object (the first function defined in Letter.js) that 
-    displays the character or an underscore and concatenate those together.
------------------------------------------------------------------------------------*/    
+    this.hangman_word = function() {
+        var hangmanLetterArray = hangman_word.split("");
+        return hangmanLetterArray;
+    };
+    /*------------------------------------------------------------------------------
+        A function that returns a string representing the word. This should call the 
+        function on each letter object (the first function defined in Letter.js) that 
+        displays the character or an underscore and concatenate those together.
+    ------------------------------------------------------------------------------*/    
     displayResult = function() {
         displayedCharacters = "";
         for (i = 0; i < hangman_word.length; i++) {
-            var newGuess = new GuessCheck(splitWord[i]);
+            var newGuess = new GuessCheck(this.splitWord[i]);
             displayedCharacters = displayedCharacters + newGuess.displayCharacter();
         }
-    },
-/*-----------------------------------------------------------------------------------
-    A function that takes a character as an argument and calls the guess function 
-    on each letter object (the second function defined in Letter.js)
------------------------------------------------------------------------------------*/    
+    };
+    /*------------------------------------------------------------------------------
+        A function that takes a character as an argument and calls the guess function 
+        on each letter object (the second function defined in Letter.js)
+    ------------------------------------------------------------------------------*/    
     LetterToMatch = function(playerChoice) {
         choice = playerChoice
         guessedLetter(choice);
         displayResult();
-    }
+    };
 
 };
 
